b1c7362b5125c7c88bb4881858fca5f268e92b65
Drew Lofland <alofland25@gmail.com>
2023-01-31 13:59:29 -0500

    finished exercise 2'

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = File.ReadAllText("appsettings.json");
+            var APIKey = JObject.Parse(key).GetValue("APIKey");
+
+            Console.WriteLine("Please enter your zip code");
+            var zipCode = Console.ReadLine();
+
+            var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?units=imperial&appid={APIKey}&zip={zipCode}";
+
+
+            Console.WriteLine($"It is currently {Weather.GetTemp(weatherURL)} degrees F in your location");
+        }
+    }
+}
---
+++ WeatherApp/Weather.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace WeatherApp
+{
+    public class Weather
+    {
+        public static double GetTemp(string weatherURL)
+        {
+            var client = new HttpClient();
+            var response = client.GetStringAsync(weatherURL).Result;
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+            return temp;
+        }
+    }
+}
